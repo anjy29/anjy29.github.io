cdc56b2d1c6612ffc348f6131963b95275809835
anjy29 <anjy.anjy2552@gmail.com>
2026-04-10 19:55:10 -0400

    Update main.js

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -22,8 +22,8 @@ const images = [
 ];
  
 
-const baseURL = "https://mdn.github.io/shared-assets/images/examples/learn/gallery/";
-//const baseURL = "./images/"
+//const baseURL = "https://mdn.github.io/shared-assets/images/examples/learn/gallery/";
+const baseURL = "./Images/"
 
 // LOOP STARTING AT 1
 
